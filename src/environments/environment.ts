856367eb02e8d4941d46440643accceb5991198f
Amit Pandit <35424309+amitpandit09@users.noreply.github.com>
2019-04-26 19:23:41 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 export const environment = {
  production: false,
  firebase: {
-   apiKey: "AIzaSyCctyNvAbkM4o2GWv-kZTG-x8cft6JbGyE",
+   apiKey: ",
    authDomain: "myfirstfireproject-e19f6.firebaseapp.com",
    databaseURL: "https://myfirstfireproject-e19f6.firebaseio.com",
    projectId: "myfirstfireproject-e19f6",
    storageBucket: "myfirstfireproject-e19f6.appspot.com",
-   messagingSenderId: "371470384843"
+   messagingSenderId: ""
  }
 };
 
